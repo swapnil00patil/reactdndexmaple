6a67f63b303fc6e0fcf94e1c6cee52da7d5382ed
swapnil00patil <swapnil00patil@gmail.com>
2018-09-10 15:57:43 +0800

    login validation added

--- src/containers/login/index.js
+++ src/containers/login/index.js
@@ -5,22 +5,46 @@ class Login extends Component {
   constructor (props) {
     super(props);
     this.state = {
-      shake: ''
+      shake: '',
+      error: ''
     }
 
-    this.submitLogin = this.submitLogin.bind(this)
+    this.submitLogin = this.submitLogin.bind(this);
+    this.usernameRef = React.createRef();
+    this.passwordRef = React.createRef();
   }
 
   submitLogin (e) {
-    this.setState({
-      shake: 'shake-it'
-    })
-    setTimeout(() => {
+    let username = this.usernameRef.current.value;
+    let password = this.passwordRef.current.value;
+
+    if(!username || !password) {
+      this.setState({
+        shake: 'shake-it'
+      })
+      setTimeout(() => {
+          this.setState({
+            shake: ''
+          }) 
+      }, 1000);
+      if(!username) {
         this.setState({
-          shake: ''
-        }) 
-    }, 1000)
-    // history.push('/home')
+          error: 'Please enter username'
+        })
+      } else if(!password) {
+        this.setState({
+          error: 'Please enter password'
+        })
+      }
+    } else if (username !== 'admin' || password !== 'admin') {
+      this.usernameRef.current.value = '';
+      this.passwordRef.current.value = '';
+      this.setState({
+        error: 'Please enter correct username and password'
+      })
+    } else {
+      history.push('/home');
+    }
     e.preventDefault();
   }
 
@@ -28,9 +52,10 @@ class Login extends Component {
     return (
       <div className={`log-form ${this.state.shake}`}>
         <h2>Login</h2>
+        { this.state.error && <div className="user-error">{this.state.error}</div> }
         <form>
-          <input type="text" title="username" placeholder="username" />
-          <input type="password" title="username" placeholder="password" />
+          <input type="text" title="username" placeholder="Username" ref={this.usernameRef} />
+          <input type="password" title="username" placeholder="Password" ref={this.passwordRef} />
           <button type="submit" className="btn" onClick={this.submitLogin}>Login</button>
         </form>
       </div>
